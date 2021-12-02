a6de40062902d04ae8830dcb386dd6080b573642
chanshiori15 <64536186+chanshiori15@users.noreply.github.com>
2021-12-02 11:44:51 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/pipo-enemy003b.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+    myImage.setAttribute ('src','pipo-enemy003b.png');
   } else {
     myImage.setAttribute ('src','images/firefox-icon.png');
   }
